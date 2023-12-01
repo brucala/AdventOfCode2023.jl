7fb576316a2a8472878ce0823ffbb2d39c88276c
brucala <brucala@gmail.com>
2023-12-01 09:31:09 +0100

    day1

--- src/day1.jl
+++ src/day1.jl
@@ -9,25 +9,46 @@ export solve1, solve2, parse_input
 ### Parse
 ###
 
-function parse_input(x::AbstractString)
-    for line in splitlines(x)
-    end
-end
+parse_input(x::AbstractString) = splitlines(x)
+
+getdigits(line) = filter(isdigit, line)
+calibration_value(digits) = toint(digits[1] * digits[end])
 
 ###
 ### Part 1
 ###
 
-function solve1(x)
-
-end
+solve1(x) = sum(calibration_value.(getdigits.(x)))
 
 ###
 ### Part 2
 ###
 
-function solve2(x)
+const DIGITS = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+const DIGITMAP = Dict(
+    [x => i for (i, x) in enumerate(DIGITS)]...,
+    [string(x) => x for x in 1:9]...
+)
 
+function calibration_value2(line)
+    ifirst, ilast = 10000, -1
+    d, u = 0, 0
+    for (k, v) in DIGITMAP
+        occursin(k, line) || continue
+        i = findfirst(k, line)[1]
+        if ifirst >= i
+            ifirst = i
+            d = v
+        end
+        i = findlast(k, line)[1]
+        if ilast <= i
+            ilast = i
+            u = v
+        end
+    end
+    return 10d + u
 end
+
+solve2(x) = sum(calibration_value2.(x))
 
 end  # module
--- test/test_day1.jl
+++ test/test_day1.jl
@@ -9,17 +9,34 @@ data = parse_input(nday)
 
 test = parse_input(
 """
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
 """ |> rstrip
 )
 
+test2 = parse_input(
+"""
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+""" |> rstrip
+)
+
+
 @testset "Day$nday tests" begin
-    @test solve1(test) == ?
-    #@test solve2(test) == ?
+    @test solve1(test) == 142
+    @test solve2(test2) == 281
 end
 
 @testset "Day$nday solutions" begin
-    @test solve1(data) == ?
-    #@test solve2(data) == ?
+    @test solve1(data) == 55488
+    @test solve2(data) == 55614
 end
 
 end  # module
