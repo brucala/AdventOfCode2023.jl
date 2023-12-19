eebef5c76de66899b1c5cef031d5c063381cfac2
brucala <brucala@gmail.com>
2023-12-19 18:33:23 +0100

    day17: heap instead of PriorityQueue

--- src/day17.jl
+++ src/day17.jl
@@ -23,70 +23,20 @@ const STEP = Dict(
     :w => (0, -1),
 )
 
-#function solve1(x)
-#    sol = typemax(Int)
-#
-#    q = [(1, 2, :e, 1, 0), (2, 1, :s, 1, 0)]
-#    i = 0
-#    seen = Dict{Tuple{Int, Int, Symbol}, Vector{Int}}()
-#    while !isempty(q)
-#        i += 1
-#        r, c, d, n, loss = pop!(q)
-#        if i % 10000 == 0
-#            @show i, r, c, d, n, loss, sol
-#            @show length(q)
-#            #@show q
-#        end
-#        i > 100000 && return q
-#        #length(q) > 40 && return
-#        checkbounds(Bool, x, r, c) || continue
-#        loss += x[r, c]
-#        loss >= sol && continue
-#
-#        k = (r, c, d)
-#        if haskey(seen, k)
-#            skip = false
-#            for i in n:3
-#                skip = seen[k][i] < loss
-#            end
-#            skip && continue
-#        else
-#            seen[k] = fill(typemax(Int), 3)
-#        end
-#        seen[k][n] = loss
-#
-#        if size(x) == (r, c)
-#            sol = min(sol, loss)
-#            println("*** got to the end")
-#            @show i, loss, sol
-#            #@show q
-#            continue
-#        end
-#        dirs = d in (:e, :w) ? (:n, :s) : (:w, :e)
-#        for newdir in dirs
-#            rr, cc = (r, c) .+ STEP[newdir]
-#            push!(q, (rr, cc, newdir, 1, loss))
-#        end
-#        if n < 3
-#            rr, cc = (r, c) .+ STEP[d]
-#            push!(q, (rr, cc, d, n+1, loss))
-#        end
-#    end
-#    return sol
-#end
-
 function solve1(x)
     sol = typemax(Int)
 
-    q = PriorityQueue(
-        (1, 2, :e, 1) => 0,
-        (2, 1, :s, 1) => 0
-    )
+    #q = PriorityQueue(
+    #    (1, 2, :e, 1) => 0,
+    #    (2, 1, :s, 1) => 0
+    #)
+    q = heapify([(0, 1, 2, :e, 1), (0, 2, 1, :s, 1)])
 
     seen = Dict{Tuple{Int, Int, Symbol, Int}, Int}()
 
     while !isempty(q)
-        k, loss = dequeue_pair!(q)
+        loss, k... = heappop!(q)
+        #k, loss = dequeue_pair!(q)
         r, c, d, n = k
 
         checkbounds(Bool, x, r, c) || continue
@@ -105,17 +55,17 @@ function solve1(x)
 
         if n < 3
             rr, cc = (r, c) .+ STEP[d]
-            haskey(q, (rr, cc, d, n+1)) && q[(rr, cc, d, n+1)] <= loss && continue
-            q[(rr, cc, d, n+1)] = loss
-            #enqueue!(q, (rr, cc, d, n+1) => loss)
+            heappush!(q, (loss, rr, cc, d, n+1))
+            #haskey(q, (rr, cc, d, n+1)) && q[(rr, cc, d, n+1)] <= loss && continue
+            #q[(rr, cc, d, n+1)] = loss
         end
 
         dirs = d in (:e, :w) ? (:s, :n) : (:e, :w)
         for newdir in dirs
             rr, cc = (r, c) .+ STEP[newdir]
-            haskey(q, (rr, cc, newdir, 1)) && q[(rr, cc, newdir, 1)] <= loss && continue
-            q[(rr, cc, newdir, 1)] = loss
-            #enqueue!(q, (rr, cc, newdir, 1) => loss)
+            heappush!(q, (loss, rr, cc, newdir, 1))
+            #haskey(q, (rr, cc, newdir, 1)) && q[(rr, cc, newdir, 1)] <= loss && continue
+            #q[(rr, cc, newdir, 1)] = loss
         end
     end
     return sol
@@ -129,15 +79,18 @@ end
 function solve2(x)
     sol = typemax(Int)
 
-    q = PriorityQueue(
-        (1, 2, :e, 1) => 0,
-        (2, 1, :s, 1) => 0
-    )
+    # a binary heap seems slightly more efficient than a priority queue
+    #q = PriorityQueue(
+    #    (1, 2, :e, 1) => 0,
+    #    (2, 1, :s, 1) => 0
+    #)
+    q = heapify([(0, 1, 2, :e, 1), (0, 2, 1, :s, 1)])
 
     seen = Dict{Tuple{Int, Int, Symbol, Int}, Int}()
 
     while !isempty(q)
-        k, loss = dequeue_pair!(q)
+        loss, k... = heappop!(q)
+        #k, loss = dequeue_pair!(q)
         r, c, d, n = k
 
         checkbounds(Bool, x, r, c) || continue
@@ -149,13 +102,6 @@ function solve2(x)
         haskey(seen, k) && seen[k] <= loss && continue
         seen[k] = loss
 
-        #if n < 4
-        #    rr, cc = (r, c) .+ STEP[d]
-        #    haskey(q, (rr, cc, d, n+1)) && q[(rr, cc, d, n+1)] <= loss && continue
-        #    q[(rr, cc, d, n+1)] = loss
-        #    continue
-        #end
-
         if size(x) == (r, c)
             sol = min(sol, loss)
             continue
@@ -163,16 +109,12 @@ function solve2(x)
 
         if n < 10
             rr, cc = (r, c) .+ STEP[d]
-            haskey(q, (rr, cc, d, n+1)) && q[(rr, cc, d, n+1)] <= loss && continue
-            q[(rr, cc, d, n+1)] = loss
+            heappush!(q, (loss, rr, cc, d, n+1))
+            #haskey(q, (rr, cc, d, n+1)) && q[(rr, cc, d, n+1)] <= loss && continue
+            #q[(rr, cc, d, n+1)] = loss
         end
 
         dirs = d in (:e, :w) ? (:s, :n) : (:e, :w)
-        #for newdir in dirs
-        #    rr, cc = (r, c) .+ STEP[newdir]
-        #    haskey(q, (rr, cc, newdir, 1)) && q[(rr, cc, newdir, 1)] <= loss && continue
-        #    q[(rr, cc, newdir, 1)] = loss
-        #end
         for newdir in dirs
             rr, cc = (r, c) .+ 4 .* STEP[newdir]
             checkbounds(Bool, x, rr, cc) || continue
@@ -183,8 +125,9 @@ function solve2(x)
                     sum(x[range(min(r,rr)+1, length=3), c])
                 end
             )
-            haskey(q, (rr, cc, newdir, 4)) && q[(rr, cc, newdir, 4)] <= newloss && continue
-            q[(rr, cc, newdir, 4)] = newloss
+            heappush!(q, (newloss, rr, cc, newdir, 4))
+            #haskey(q, (rr, cc, newdir, 4)) && q[(rr, cc, newdir, 4)] <= newloss && continue
+            #q[(rr, cc, newdir, 4)] = newloss
         end
     end
     return sol
